edd6fccb7aa34a81bea676b173b7fe4a950f0af5
Carlos Bartolo <carlosbartolo0615@gmail.com>
2024-03-06 19:22:30 -0700

    SQL Alchemy and alembic

---
+++ api/model.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import relationship, DeclarativeBase, MappedAsDataclass, Mapped, mapped_column
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    email = Mapped[str]
+    hashed_password = Mapped[str]
