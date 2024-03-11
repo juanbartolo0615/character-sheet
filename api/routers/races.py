47c6ab663a70af79a7d6674bbe6e21e43c07f361
Carlos Bartolo <carlosbartolo0615@gmail.com>
2024-03-11 12:10:52 -0600

    Added get a single race route and cleaned p the code

--- api/routers/races.py
+++ api/routers/races.py
@@ -11,7 +11,6 @@ class SubRacesTraitsOut(BaseModel):
     trait: str
     description: str
 
-
 class SubRacesOut(BaseModel):
     id: int
     race_id: int
@@ -34,41 +33,54 @@ class RaceOut(BaseModel):
 
 router = APIRouter(prefix="/races", tags=["races"])
 
+
+def get_racial_traits(race_id: int):
+    with SessionLocal() as conn:
+        traits = conn.execute(select(RacialTraits).where(RacialTraits.race_id == race_id))
+        racial_traits = []
+        for trait in traits.scalars():
+            racial_traits.append(RacialTraitsOut(
+                id=trait.id,
+                race_id=trait.race_id,
+                trait=trait.trait,
+                description=trait.description
+            ))
+    return racial_traits
+
+def get_subraces(race_id: int):
+    with SessionLocal() as conn:
+        subraces = conn.execute(select(Subraces).where(Subraces.race_id == race_id))
+        subraces_result = []
+        for subrace in subraces.scalars():
+            subrace_traits = conn.execute(select(SubracesTraits).where(SubracesTraits.subrace_id == subrace.id))
+            subrace_traits_result = []
+            for trait in subrace_traits.scalars():
+                subrace_traits_result.append(SubRacesTraitsOut(
+                    id=trait.id,
+                    subrace_id=trait.subrace_id,
+                    trait=trait.trait,
+                    description=trait.description
+                ))
+            subraces_result.append(SubRacesOut(
+                id=subrace.id,
+                race_id=subrace.race_id,
+                name=subrace.name,
+                traits=subrace_traits_result
+            ))
+        if not subraces_result:
+            return None
+        else:
+            return subraces_result
+
+
 @router.get("/", response_model=List[RaceOut])
 def get_all_races():
     with SessionLocal() as conn:
         races = conn.execute(select(Races))
         result = []
         for race in races.scalars():
-            traits = conn.execute(select(RacialTraits).where(RacialTraits.race_id == race.id))
-            racial_traits = []
-            for trait in traits.scalars():
-                racial_traits.append(RacialTraitsOut(
-                    id=trait.id,
-                    race_id=trait.race_id,
-                    trait=trait.trait,
-                    description=trait.description
-                ))
-
-            subraces = conn.execute(select(Subraces).where(Subraces.race_id == race.id))
-            subraces_result = []
-            if subraces:
-                for subrace in subraces.scalars():
-                    subrace_traits = conn.execute(select(SubracesTraits).where(SubracesTraits.subrace_id == subrace.id))
-                    subrace_traits_result = []
-                    for trait in subrace_traits.scalars():
-                        subrace_traits_result.append(SubRacesTraitsOut(
-                            id=trait.id,
-                            subrace_id=trait.subrace_id,
-                            trait=trait.trait,
-                            description=trait.description
-                        ))
-                    subraces_result.append(SubRacesOut(
-                        id=subrace.id,
-                        race_id=subrace.race_id,
-                        name=subrace.name,
-                        traits=subrace_traits_result
-                    ))
+            racial_traits = get_racial_traits(race.id)
+            subraces_result = get_subraces(race.id)
 
             result.append(RaceOut(
                 id=race.id,
@@ -78,3 +90,18 @@ def get_all_races():
                 subraces=subraces_result
             ))
     return result
+
+@router.get("/{race_id}")
+def get_one_race(race_id: int):
+    with SessionLocal() as conn:
+        race = conn.execute(select(Races).where(Races.id == race_id)).scalar_one()
+        racial_traits = get_racial_traits(race_id)
+        subraces_result = get_subraces(race_id)
+
+        return RaceOut(
+                id=race.id,
+                name=race.name,
+                description=race.description,
+                traits=racial_traits,
+                subraces=subraces_result
+            )
